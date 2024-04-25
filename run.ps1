d17b12f970b6b437ac6c761f548f7eb2959fe26c
turtle <769711153@qq.com>
2024-04-25 19:10:35 +0800

    给脚本加上校验，时间戳也加上秒

--- run.ps1
+++ run.ps1
@@ -1,6 +1,18 @@
-$prefix = Get-Date -Format "MM-dd-yyyy HH-mm"
+if( -not ($PSVersionTable.PSVersion.Major -ge 7 )){
+    Write-Host "发现错误：" -ForegroundColor White -BackgroundColor Red
+    Write-Host ""
+    Write-Host "Powershell版本要求大于7.0。请先升级你的版本。程序现在退出。" -ForegroundColor White -BackgroundColor Red
+    Write-Host ""
+    exit
+    Write-Output "After Exit"
+}
+$prefix = Get-Date -Format "MM-dd-yyyy-HH-mm-ss"
 
 $Env:BCEM_OUTPUT_PREFIX=$prefix
+
+Write-Host "现在启动若干个节点。输出前缀是时间戳。: $prefix" -ForegroundColor White -BackgroundColor Green
+Write-Host ""
+
 
 start cmd '/k .\blockchainEmulator.exe -n 1'
 
