fbbef03ce1050c5254fe4ef5c884ce58550a2717
turtle <769711153@qq.com>
2024-04-25 18:59:14 +0800

    补一个powershell启动脚本。现在能正常运行。

--- run.ps1
+++ run.ps1
@@ -1,8 +1,12 @@
-start cmd /k .\blockchainEmulator.exe -n 1   
+$prefix = Get-Date -Format "MM-dd-yyyy HH-mm"
 
-start cmd /k .\blockchainEmulator.exe -n 2  
+$Env:BCEM_OUTPUT_PREFIX=$prefix
 
-start cmd /k .\blockchainEmulator.exe -c -f
+start cmd '/k .\blockchainEmulator.exe -n 1'
 
-start cmd /k .\blockchainEmulator.exe -n 0  
+start cmd '/k .\blockchainEmulator.exe -n 2'
+
+start cmd '/k .\blockchainEmulator.exe -c -f'
+
+start cmd '/k .\blockchainEmulator.exe -n 0'
 
