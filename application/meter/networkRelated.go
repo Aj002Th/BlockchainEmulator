5c392a637b1d20b2b2b2a42adcdff98e5f9254b4
turtle <769711153@qq.com>
2024-04-29 05:00:58 +0800

    测度模块删掉冗余代码

--- application/meter/networkRelated.go
+++ application/meter/networkRelated.go
@@ -2,14 +2,6 @@ package meter
 
 import "github.com/Aj002Th/BlockchainEmulator/network"
 
-func onUpload(n int) {
-
-}
-
-func onDownload(n int) {
-
-}
-
 var TotalUpload int
 var TotalDownload int
 
