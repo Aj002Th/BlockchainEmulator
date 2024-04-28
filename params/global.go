739f8eb6fc5b07302fa6126b419fcc1f6114da19
turtle <769711153@qq.com>
2024-04-29 05:15:06 +0800

    改变度量配置方式

--- params/global.go
+++ params/global.go
@@ -4,4 +4,9 @@ var (
 	IPmap_nodeTable  = make(map[uint64]map[uint64]string)
 	MeasureRelayMod  = []string{"TPS_Relay", "TCL_Relay", "CrossTxRate_Relay", "TxNumberCount_Relay"}
 	Enabled_Measures = []string{"CPU", "Disk", "TPS", "TCL", "TxCount"}
+	Meter_CPU        = true // 控制输出的度量
+	Meter_Disk       = true
+	Meter_TPS        = true
+	Meter_TCL        = true
+	Meter_TxCount    = true
 )
