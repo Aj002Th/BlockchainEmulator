2f40ac48c0f610d75a3cd98926886e0b471c8b3f
turtle <769711153@qq.com>
2024-04-30 04:01:43 +0800

    注释掉错误的饭

--- application/supervisor/metrics/api.go
+++ application/supervisor/metrics/api.go
@@ -1,43 +1,35 @@
 package metrics
 
-import (
-	"fmt"
+// var metrics []Metrics
 
-	"github.com/Aj002Th/BlockchainEmulator/application/comm"
-	"github.com/Aj002Th/BlockchainEmulator/params"
-	"github.com/Aj002Th/BlockchainEmulator/signal"
-)
+// func StartHandle() {
+// 	// 先把模块注册上来
+// 	for _, v := range params.Enabled_Measures {
+// 		m := GetMetricByName(v)
+// 		metrics = append(metrics, m)
+// 	}
+// 	// 然后开始监听。听到就告诉各个模块处理。
+// 	comm.Listen()
+// 	m := comm.Recv()
+// 	for _, v := range metrics {
+// 		v.OnBlockInfoMsg(m)
+// 	}
+// }
 
-var metrics []Metrics
+// func GetMetricByName(name string) Metrics {
+// 	switch name {
+// 	case "TPS":
+// 		return BlockCountMetric()
+// 	default:
+// 		panic("")
+// 	}
+// }
 
-func StartHandle() {
-	// 先把模块注册上来
-	for _, v := range params.Enabled_Measures {
-		m := GetMetricByName(v)
-		metrics = append(metrics, m)
-	}
-	// 然后开始监听。听到就告诉各个模块处理。
-	comm.Listen()
-	m := comm.Recv()
-	for _, v := range metrics {
-		v.OnBlockInfoMsg(m)
-	}
-}
+// func DumpAll() {
+// 	for _, v := range metrics {
+// 		desc := v.GetResult()
+// 		fmt.Printf("desc: %v\n", desc)
+// 	}
+// }
 
-func GetMetricByName(name string) Metrics {
-	switch name {
-	case "TPS":
-		return BlockCountMetric()
-	default:
-		panic("")
-	}
-}
-
-func DumpAll() {
-	for _, v := range metrics {
-		desc := v.GetResult()
-		fmt.Printf("desc: %v\n", desc)
-	}
-}
-
-var OnDump signal.Signal[string]
+// var OnDump signal.Signal[string]
