a94b033cb7570953c122522012107761b9d526a1
turtle <769711153@qq.com>
2024-05-07 13:02:40 +0800

    定义一组合理的preset预设启动值

---
+++ run-preset.ps1
@@ -0,0 +1,19 @@
+﻿param (
+    [Parameter(Mandatory=$false)]
+    [int]$Select = 3 # Parameter to specify the number of times to execute the command
+)
+
+switch ($Select){
+    0 {
+        # 小小的批次
+        .\run.ps1 -Args "--TotalDataSize 16000 --BatchSize 4000"
+    }
+    1{
+        # 小小的批次 + 降低了Inject Speed
+        .\run.ps1 -Args "--TotalDataSize 16000 --BatchSize 4000 --InjectSpeed 1000"
+    }
+    2{
+        #  小批次，四个节点
+        .\run.ps1 -Args "--TotalDataSize 16000 --BatchSize 4000" -N 4
+    }
+}
